93be4bc23533d8538523820e330be418b8f16494
firstmetcs <824311265@qq.com>
2025-07-14 15:46:46 +0800

    feat: improve baidu

--- plugin/script/baidu_search_remove_ads.js
+++ plugin/script/baidu_search_remove_ads.js
@@ -1,10 +1,34 @@
+const Injection = `var css = {
+    baidu_search: "div[style*=fixed],.ec_ad_results {display:none !important;} ", // baidu
+    baidu_index: "a[data-tclog] > img, #login-wraps, div[style*='overflow'], .rn-container, .s-loading-frame.bottom {display:none !important;}",
+}
+        
 
-// CSS and JS resources for ad removal
-const JS_URL = "https://firstmetcs.net/Adguard/baidu.rm.ads.js";
+// 动态创建引用内部资源 内嵌式样式 内嵌式脚本
+function css_adsRemove(newstyle, delaytime, id) {
+    setTimeout(() => {
+        var creatcss = document.createElement("style");
+        creatcss.id = id;
+        creatcss.innerHTML = newstyle;
+        document.getElementsByTagName('head')[0].appendChild(creatcss)
+        console.log("CSS样式新增完毕！");
+    }, delaytime);
+}
+
+
+let regex = /https?:\\/\\/(www|m)\\.baidu\\.com\\/(from=|s\\?)/gi
+if (window.location.href.search(regex) !== -1) {
+    css_adsRemove(css.baidu_search);
+    console.log('移除搜索结果广告🪧...')
+} else {
+    // adsDomain_switch("zhidao")
+    css_adsRemove(css.baidu_index);
+    console.log('移首页广告🪧...')
+}`
 
 // Regular expressions for URL matching
 const TITLE_INJECTION = `</title>
-<script type="text/javascript" async="async" src="${JS_URL}"></script>
+<script type="text/javascript">${Injection}</script>
 `;
 
 // Regular expressions for content manipulation
